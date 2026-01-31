930613ae69e2d24eb6af13bc5a1339472699f57f
Bagavathi Samiappan <sbagavathi2006@gmail.com>
2026-01-31 16:21:50 -0500

    Created Bagavathi Branch

---
+++ src/test/java/stepDefinition/OnBoardingStepDefinition.java
@@ -0,0 +1,42 @@
+package stepDefinition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class OnBoardingStepDefinition {
+	
+	@Given("I want to write a step with name1")
+	public void i_want_to_write_a_step_with_name1() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I check for the {int} in step")
+	public void i_check_for_the_in_step(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("I verify the success in step")
+	public void i_verify_the_success_in_step() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("I want to write a step with name2")
+	public void i_want_to_write_a_step_with_name2() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("I verify the Fail in step")
+	public void i_verify_the_fail_in_step() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+
+
+}

89c5073973f27254224781d8ea2d5fc71493dd95
mathumathi0989 <mathumathi.b@gmail.com>
2026-02-05 22:16:40 -0500

    few dashboard features completed

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -13,7 +13,7 @@ import io.cucumber.testng.CucumberOptions;
 				  "json:target/cucumber-reports/Team01_Selenium_Titans.json", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
 		monochrome = false,
 
-		//tags = "@checking",
+	//	tags = "@checking",
 		features = {"src/test/resources/features/"},
 		glue = {"stepDefinition"})
 
